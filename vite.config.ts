13b2cb9d226993565b8ebee56b06c1bead7ea980
Matej Stieranka <matej@stieranka.eu>
2026-05-02 17:32:35 +0200

    fix(ci): remove unneeded Cloudflare Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte(), tailwindcss(), cloudflare()],
+	plugins: [svelte(), tailwindcss()],
 });
