7d36663fd0d41c200a0853ca1f76e5ce38ed4b8e
Matej Stieranka <matej@stieranka.eu>
2026-05-02 13:25:47 +0200

    feat: add 404 page

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import Cleaner from './routes/Cleaner.svelte';
 import Arena from './routes/Arena.svelte';
 import Gallery from './routes/Gallery.svelte';
 import Layout from './routes/layout.svelte';
+import PageNotFound from './routes/404.svelte';
 
 export const { p, navigate, isActive, route } = createRouter({
 	'/': Home,
@@ -12,5 +13,6 @@ export const { p, navigate, isActive, route } = createRouter({
     '/cleaner': Cleaner,
     '/arena': Arena,
     '/gallery': Gallery,
+    '*': PageNotFound,
     layout: Layout,
 });
---
+++ src/routes/404.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>404 | Style Extractor</title>
+</svelte:head>
+
+<main class="container mx-auto p-4">
+  <h1 class="text-2xl font-bold mb-4">Page Not Found</h1>
+  <p class="mb-4">Sorry, the page you're looking for doesn't exist.</p>
+  <a href="/" class="text-purple-600 hover:underline">Go back to Home</a>
+</main>
